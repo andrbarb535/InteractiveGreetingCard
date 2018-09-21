01f6a40e5c4b1000ff97ffaa1ef2ad6fe565b584
Andrew Barber <andrbarb535@ed.amdsb.ca>
2018-09-21 15:01:52 -0400

    Add project files.

---
+++ InteractiveGreetingCard/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace InteractiveGreetingCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Next = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(100, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ORION THE HUNTER";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Next
+            // 
+            this.Next.BackColor = System.Drawing.Color.Transparent;
+            this.Next.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Next.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Next.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Next.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Next.ForeColor = System.Drawing.Color.White;
+            this.Next.Location = new System.Drawing.Point(480, 564);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(92, 35);
+            this.Next.TabIndex = 1;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = false;
+            this.Next.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 611);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Next;
+    }
+}
+
---
+++ InteractiveGreetingCard/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+/// <summary>
+/// Created By: Andrew Barber
+/// Date: 2018-09-20
+/// Description: Interactive Constillation Brochure
+/// </summary>
+
+namespace InteractiveGreetingCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Black, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            SolidBrush drawBrush2 = new SolidBrush(Color.Yellow);
+            Font drawFont = new Font("Imprint MT Shadow", 42, FontStyle.Bold);
+            SolidBrush drawBrush3 = new SolidBrush(Color.Yellow);
+            Random randGen = new Random();
+
+            int h = 10;
+            int w = 10;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                int rx, ry;
+
+                rx = randGen.Next(1, this.Width);
+                ry = randGen.Next(1, this.Height);
+
+                g.FillEllipse(drawBrush, rx, ry, h, w);
+            }
+
+            g.FillEllipse(drawBrush, 150, 550, 10, 10);
+            //orion right foot
+
+            g.FillEllipse(drawBrush, 300, 520, 10, 10);
+            //orion left foot
+            
+            g.FillEllipse(drawBrush, 200, 400, 10, 10);
+            g.FillEllipse(drawBrush, 220, 390, 10, 10);
+            g.FillEllipse(drawBrush, 240, 375, 10, 10);
+            //orion belt
+            
+            g.FillEllipse(drawBrush, 125, 195, 10, 10);
+            g.FillEllipse(drawBrush, 230, 150, 10, 10);
+            g.FillEllipse(drawBrush, 275, 200, 10, 10);
+            //orion torso
+            
+            g.FillEllipse(drawBrush, 75, 145, 10, 10);
+            g.FillEllipse(drawBrush, 20, 10, 10, 10);
+            //orion left arm
+            
+            g.FillEllipse(drawBrush, 475, 10, 10, 10);
+            g.FillEllipse(drawBrush, 540, 50, 10, 10);
+            g.FillEllipse(drawBrush, 570, 250, 10, 10);
+            g.FillEllipse(drawBrush, 525, 325, 10, 10);
+            //orion bow
+
+            for (float x = 600; x >= 450; x = x - 2)
+            {
+                g.DrawEllipse(drawPen, x, 190, 10, 10);
+                g.FillEllipse(drawBrush, x, 190, 10, 10);
+                //orion bowstring
+
+                Thread.Sleep(100);
+            }
+            
+            //this.Refresh();
+            
+            Pen drawPen2 = new Pen(Color.Green, 1);
+
+            g.DrawLine(drawPen2, 150, 550, 300, 520);
+            g.DrawLine(drawPen2, 150, 550, 200, 400);
+            g.DrawLine(drawPen2, 200, 400, 220, 390);
+            g.DrawLine(drawPen2, 220, 390, 240, 377);
+            g.DrawLine(drawPen2, 240, 377, 300, 520);
+            //constelation lower body
+
+            g.DrawLine(drawPen2, 240, 377, 275, 200);
+            g.DrawLine(drawPen2, 275, 200, 230, 150);
+            g.DrawLine(drawPen2, 230, 150, 125, 195);
+            g.DrawLine(drawPen2, 125, 195, 200, 400);
+            //constelation upper body
+
+            g.DrawLine(drawPen2, 125, 195, 75, 145);
+            g.DrawLine(drawPen2, 75, 145, 20, 10);
+            //constelation left arm
+
+            g.DrawLine(drawPen2, 475, 10, 540, 50);
+            g.DrawLine(drawPen2, 540, 50, 450, 190);
+            g.DrawLine(drawPen2, 450, 190, 570, 250);
+            g.DrawLine(drawPen2, 570, 250, 525, 325);
+            //constelation bow
+
+            g.DrawLine(drawPen2, 450, 190, 275, 200);
+            //constelation left arm
+
+            Next.Visible = false;
+        }
+    }
+}
